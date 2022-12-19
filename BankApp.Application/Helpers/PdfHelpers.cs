4cfa3210d04402de78d160a6e3fa15254b62ca23
JohnnyK99 <jankolodziejczyk99@gmail.com>
2022-12-20 00:05:55 +0100

    Improved confirmation pdf design

--- BankApp.Application/Helpers/PdfHelpers.cs
+++ BankApp.Application/Helpers/PdfHelpers.cs
@@ -14,10 +14,10 @@ namespace BankApp.Application.Helpers
 
         public PdfHelpers()
         {
-            linePen = new(XColor.FromName("gray"));
-            fontBold = new("Arial", 20, XFontStyle.Bold);
-            fontNormal = new("Arial", 14, XFontStyle.Regular);
-            fontLabel = new("Arial", 12, XFontStyle.Regular);
+            linePen = new(XColor.FromKnownColor(XKnownColor.Gray));
+            fontBold = new("Calibri", 20, XFontStyle.Bold);
+            fontNormal = new("Calibri", 14, XFontStyle.Regular);
+            fontLabel = new("Calibri", 12, XFontStyle.Regular);
         }
 
         public Stream GetTransactionConfirmation(Transaction transaction)
@@ -27,7 +27,7 @@ namespace BankApp.Application.Helpers
             XGraphics gfx = XGraphics.FromPdfPage(page);
 
             gfx.DrawString("Bank App", fontBold, XBrushes.Black, new XRect(20, 0, page.Width-20, 50), XStringFormats.CenterLeft);
-            gfx.DrawLine(linePen, new XPoint(0, 55), new XPoint(page.Width, 55));
+            gfx.DrawLine(linePen, new XPoint(30, 55), new XPoint(page.Width-30, 55));
             gfx.DrawString("Potwierdzenie transakcji", fontBold, XBrushes.Black, new XRect(20, 55, page.Width-20, 40), XStringFormats.Center);
 
             gfx.DrawString("Nadawca", fontLabel, XBrushes.Black, new XRect(20, 95, (page.Width - 20)/2, 40), XStringFormats.CenterLeft);
@@ -39,14 +39,14 @@ namespace BankApp.Application.Helpers
             gfx.DrawString("Tytuł", fontLabel, XBrushes.Black, new XRect(20, 175, (page.Width - 20) / 2, 40), XStringFormats.CenterLeft);
             gfx.DrawString(transaction.Title, fontNormal, XBrushes.Black, new XRect((page.Width - 20) / 2, 175, page.Width - 20, 40), XStringFormats.CenterLeft);
 
-            gfx.DrawString("Opis", fontLabel, XBrushes.Black, new XRect(20, 225, (page.Width - 20) / 2, 40), XStringFormats.CenterLeft);
-            gfx.DrawString(transaction.Description, fontNormal, XBrushes.Black, new XRect((page.Width - 20) / 2, 225, page.Width - 20, 40), XStringFormats.CenterLeft);
+            gfx.DrawString("Opis", fontLabel, XBrushes.Black, new XRect(20, 215, (page.Width - 20) / 2, 40), XStringFormats.CenterLeft);
+            gfx.DrawString(transaction.Description, fontNormal, XBrushes.Black, new XRect((page.Width - 20) / 2, 215, page.Width - 20, 40), XStringFormats.CenterLeft);
 
-            gfx.DrawString("Data transakcji", fontLabel, XBrushes.Black, new XRect(20, 265, (page.Width - 20) / 2, 40), XStringFormats.CenterLeft);
-            gfx.DrawString(transaction.Date.ToString("dd.MM.yyyy HH:mm:ss"), fontNormal, XBrushes.Black, new XRect((page.Width - 20) / 2, 265, page.Width - 20, 40), XStringFormats.CenterLeft);
+            gfx.DrawString("Data transakcji", fontLabel, XBrushes.Black, new XRect(20, 255, (page.Width - 20) / 2, 40), XStringFormats.CenterLeft);
+            gfx.DrawString(transaction.Date.ToString("dd.MM.yyyy HH:mm:ss"), fontNormal, XBrushes.Black, new XRect((page.Width - 20) / 2, 255, page.Width - 20, 40), XStringFormats.CenterLeft);
 
-            gfx.DrawString("Kwota", fontLabel, XBrushes.Black, new XRect(20, 305, (page.Width - 20) / 2, 40), XStringFormats.CenterLeft);
-            gfx.DrawString($"{transaction.Amount} zł", fontNormal, XBrushes.Black, new XRect((page.Width - 20) / 2, 305, page.Width - 20, 40), XStringFormats.CenterLeft);
+            gfx.DrawString("Kwota", fontLabel, XBrushes.Black, new XRect(20, 295, (page.Width - 20) / 2, 40), XStringFormats.CenterLeft);
+            gfx.DrawString($"{transaction.Amount} zł", fontNormal, XBrushes.Black, new XRect((page.Width - 20) / 2, 295, page.Width - 20, 40), XStringFormats.CenterLeft);
 
             Stream stream = new MemoryStream();
             document.Save(stream);
