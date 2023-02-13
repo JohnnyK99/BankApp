a7b050c31557fb13db040c8e7ff8f59995cd9758
JohnnyK99 <jankolodziejczyk99@gmail.com>
2023-02-13 19:34:32 +0100

    Added missing translations & adjusted transactions constants

--- BankApp.SPA/projects/spa/src/app/shared/constants/transactions.constants.ts
+++ BankApp.SPA/projects/spa/src/app/shared/constants/transactions.constants.ts
@@ -2,6 +2,6 @@ export const TransactionsConstants = {
   accountNumberMask: '00 0000 0000 0000 0000 0000 0000',
   accountNumberRegex: '\\d{26}|\\d{2}[ ]\\d{4}[ ]\\d{4}[ ]\\d{4}[ ]\\d{4}[ ]\\d{4}[ ]\\d{4}',
   minTransactionAmount: 0.01,
-  defaultPageSize: 15,
-  pageSizeOptions: [15, 30, 50],
+  defaultPageSize: 10,
+  pageSizeOptions: [10, 15, 30, 50],
 };
